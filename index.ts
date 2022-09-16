493ec2337a3dcebf29c5341337480f0a3887c3ff
Pyunghyuk Yoo <62975595+purple-lion@users.noreply.github.com>
2022-09-16 21:01:52 +0900

    ✨ UPDATE: refactored

--- index.ts
+++ index.ts
@@ -1,46 +1,27 @@
+import { createStatementData } from "./createStatementData";
+import plays from "./plays.json";
+import invoices from "./invoices.json";
+
 function statement(invoice: any, plays: any) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat("en-US", {
+  return renderPlainText(createStatementData(invoice, plays), plays);
+}
+
+function usd(aNumber: number) {
+  return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
     minimumFractionDigits: 2,
-  }).format;
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = 0;
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${
-      perf.audience
-    }석\n`;
-    totalAmount += thisAmount;
-  }
-
-  result += `총액: ${format(totalAmount / 100)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
-  return result;
+  }).format(aNumber / 100);
 }
 
-import plays from "./plays.json";
-import invoices from "./invoices.json";
+function renderPlainText(data: any, plays: any) {
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
+  for (let perf of data.performances) {
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+  }
+  result += `총액: ${usd(data.totalAmount)}\n`;
+  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+  return result;
+}
 
 console.log(statement(invoices[0], plays));
