bac5695e2966bb91572cb981aaf537c2ab6b613e
Pyunghyuk Yoo <62975595+purple-lion@users.noreply.github.com>
2022-09-17 12:56:17 +0900

    1663386977

--- index.ts
+++ index.ts
@@ -16,19 +16,24 @@ interface IPlay {
 type IPlays = Record<string, IPlay>;
 
 function statement(invoice: IInvoice, plays: IPlays) {
-  let totalAmount = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석\n`;
-    totalAmount += amountFor(perf);
   }
-
-  result += `총액: ${usd(totalAmount)}\n`;
+  result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
 
   function totalVolumeCredits() {
     let volumeCredits = 0;
