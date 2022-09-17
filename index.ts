70c30178f74000e2df6a412f34e7aa8b363c0967
Pyunghyuk Yoo <62975595+purple-lion@users.noreply.github.com>
2022-09-17 12:51:13 +0900

    1663386673

--- index.ts
+++ index.ts
@@ -19,23 +19,27 @@ function statement(invoice: IInvoice, plays: IPlays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
+
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
 
-    result += ` ${playFor(perf).name}: ${format(
-      amountFor(perf, playFor(perf)) / 100
+    result += ` ${playFor(perf).name}: ${usd(
+      amountFor(perf, playFor(perf))
     )} (${perf.audience}석\n`;
     totalAmount += amountFor(perf, playFor(perf));
   }
 
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
+
+  function usd(aNumber: number) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100);
+  }
 
   function volumeCreditsFor(perf: IPerformance) {
     let result = 0;
