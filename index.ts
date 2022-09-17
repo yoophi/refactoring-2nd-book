84744f610bb7cc053d9cbefa8a070451625c4950
Pyunghyuk Yoo <62975595+purple-lion@users.noreply.github.com>
2022-09-17 13:58:23 +0900

    1663390703

--- index.ts
+++ index.ts
@@ -40,15 +40,64 @@ function statement(invoice: IInvoice, plays: IPlays) {
     result.totalAmount = totalAmount(result);
     result.totalVolumeCredits = totalVolumeCredits(result);
     return result as IStatementData;
-  }
 
-  function enrichPerformance(performance: IPerformance) {
-    const result: any = Object.assign({}, performance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
+    function enrichPerformance(performance: IPerformance) {
+      const result: any = Object.assign({}, performance);
+      result.play = playFor(result);
+      result.amount = amountFor(result);
+      result.volumeCredits = volumeCreditsFor(result);
 
-    return result as IEnrichPerformance;
+      return result as IEnrichPerformance;
+    }
+
+    function playFor(aPerformance: IPerformance) {
+      return plays[aPerformance.playID];
+    }
+
+    function amountFor(aPerformance: IPerformance & { play: IPlay }) {
+      let result = 0;
+      switch (aPerformance.play.type) {
+        case "tragedy":
+          result = 40000;
+          if (aPerformance.audience > 30) {
+            result += 1000 * (aPerformance.audience - 30);
+          }
+          break;
+        case "comedy":
+          result = 30000;
+          if (aPerformance.audience > 20) {
+            result += 10000 + 500 * (aPerformance.audience - 20);
+          }
+          break;
+        default:
+          throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
+      }
+      return result;
+    }
+
+    function volumeCreditsFor(perf: IPerformance & { play: IPlay }) {
+      let result = 0;
+      result += Math.max(perf.audience - 30, 0);
+      if ("comedy" === perf.play.type) result += Math.floor(perf.audience / 5);
+
+      return result;
+    }
+
+    function totalAmount(data: { performances: IEnrichPerformance[] }) {
+      let result = 0;
+      for (let perf of data.performances) {
+        result += perf.amount;
+      }
+      return result;
+    }
+
+    function totalVolumeCredits(data: { performances: IEnrichPerformance[] }) {
+      let volumeCredits = 0;
+      for (let perf of data.performances) {
+        volumeCredits += perf.volumeCredits;
+      }
+      return volumeCredits;
+    }
   }
 
   function renderPlainText(
@@ -74,55 +123,6 @@ function statement(invoice: IInvoice, plays: IPlays) {
         minimumFractionDigits: 2,
       }).format(aNumber / 100);
     }
-  }
-
-  function playFor(aPerformance: IPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance: IPerformance & { play: IPlay }) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
-    return result;
-  }
-
-  function volumeCreditsFor(perf: IPerformance & { play: IPlay }) {
-    let result = 0;
-    result += Math.max(perf.audience - 30, 0);
-    if ("comedy" === perf.play.type) result += Math.floor(perf.audience / 5);
-
-    return result;
-  }
-
-  function totalAmount(data: { performances: IEnrichPerformance[] }) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
-  }
-
-  function totalVolumeCredits(data: { performances: IEnrichPerformance[] }) {
-    let volumeCredits = 0;
-    for (let perf of data.performances) {
-      volumeCredits += perf.volumeCredits;
-    }
-    return volumeCredits;
   }
 }
 
