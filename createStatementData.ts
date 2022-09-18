e35eb1a60473de1fa94c687ecb8b028753f91edf
Pyunghyuk Yoo <62975595+purple-lion@users.noreply.github.com>
2022-09-18 12:26:54 +0900

    1663471614

--- createStatementData.ts
+++ createStatementData.ts
@@ -10,39 +10,47 @@ import {
 class PerformanceCalculator {
   constructor(public performance: IPerformance, public play: IPlay) {}
 
-  get amount() {
-    let result = 0;
-    switch (this.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${this.play.type}`);
-    }
-    return result;
+  get amount(): number | undefined {
+    throw new Error("서브클래스에서 처리하도록 설계되었습니다.");
   }
 
   get volumeCredits() {
-    let result = 0;
-    result += Math.max(this.performance.audience - 30, 0);
-    if ("comedy" === this.play.type)
-      result += Math.floor(this.performance.audience / 5);
+    return Math.max(this.performance.audience - 30, 0);
+  }
+}
 
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
     return result;
   }
 }
 
-function createPerformanceCalculator(performance: IPerformance, aPlay: IPlay) {
-  return new PerformanceCalculator(performance, aPlay);
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    return result;
+  }
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
+  }
+}
+
+function createPerformanceCalculator(aPerformance: IPerformance, aPlay: IPlay) {
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+  }
 }
 
 export function createStatementData(
@@ -63,7 +71,7 @@ export function createStatementData(
     );
 
     const result: any = Object.assign({}, aPerformance);
-    result.play = playFor(result);
+    result.play = calculator.play;
     result.amount = calculator.amount;
     result.volumeCredits = calculator.volumeCredits;
 
